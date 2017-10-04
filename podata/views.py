7b200afb44a905e678004d27010b7793ce69f08f
Vineet Menon <mvineetmenon@gmail.com>
2017-10-04 16:09:03 +0530

    implemented sorting and pagination

--- podata/views.py
+++ podata/views.py
@@ -14,9 +14,31 @@ def index(request):
 
 def get_postal_data(request):
     post_request = request.POST
+    print post_request
+
+    postreqkey = post_request.keys()
+    postreqkey.sort()
+    # print postreqkey
+
+    ordering_direction = post_request['order[0][dir]']
+    ordering_column = post_request['order[0][column]']
+    ordering_field = post_request['columns['+ordering_column+'][data]']
+    print "XOXOXOXOXOXOXOXO", ordering_field
+
+
+    data_table_len = post_request['length']
+    datatable_offset = post_request['start']
 
     posturi = "https://api.data.gov.in/resource/0a076478-3fd3-4e2c-b2d2-581876f56d77?format=json&api-key=579b464db66ec23bdd000001d15d144d27ca4c3b702b8a67f865eb17"
+    posturi += "&length=" + data_table_len
+    posturi += "&offset=" + datatable_offset
+    posturi += "&sort[" + ordering_field + "]=" + ordering_direction
     # posturi = posturi + "&filters[pincode]=441906"
+
+    # Flag to indicate if filtering of the data has been carried out
+    filtered = False
+
+    print posturi
 
     proxy = urllib2.ProxyHandler({'https':"http://mvineet:Ampersand&7@proxy.barc.gov.in:8080"})
     auth = urllib2.HTTPBasicAuthHandler()
@@ -26,13 +48,16 @@ def get_postal_data(request):
     response = urllib2.urlopen(posturi)
 
     jsonresponse =  json.load(response)
-    print json.dumps(jsonresponse, indent=4)
+    # print json.dumps(jsonresponse, indent=4)
 
     datatable_response = {
-        'draw': int(post_request.get("draw")), 
-        'recordsTotal': jsonresponse.get("total"), 
-        'recordsFiltered': jsonresponse.get("count"), 
-        'data': jsonresponse.get('records')
+        'draw': int(post_request["draw"]), 
+        'recordsTotal': jsonresponse["total"], 
+        'recordsFiltered': jsonresponse["total"], 
+        'data': jsonresponse["records"]
     }
+
+    if filtered:
+        datatable_response['recordsFiltered'] = 10
 
     return JsonResponse(datatable_response)
